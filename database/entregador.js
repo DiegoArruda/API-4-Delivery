9312324d28a80a38c37f4706b7d97e5117f69cba
Diego Arruda <arrudadiego1@gmail.com>
2023-04-19 15:15:00 -0300

    CRUD de pedidos completo e CRUD de entregadores quase completo

---
+++ database/entregador.js
@@ -0,0 +1,26 @@
+const { DataTypes } = require("sequelize");
+const { connection } = require("./database");
+
+const Entregador = connection.define(
+  "Entregador",
+  {
+    nome: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [0, 255],
+      },
+    },
+    telefone: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+      validate: {
+        isNumeric: true,
+      },
+    },
+  },
+  { paranoid: true, deletedAt: "destroyTime" }
+);
+
+module.exports = Entregador;
---
+++ database/pedido.js
@@ -0,0 +1,36 @@
+const { DataTypes } = require("sequelize");
+const { connection } = require("./database");
+const Entregador = require("./entregador");
+
+const Pedido = connection.define(
+  "Pedido",
+  {
+    descricao: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [0, 100], // Verifica se a string tem de 0 a 100 caracteres
+      },
+    },
+    endereco_entrega: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true, // Verifica se o valor não é vazio
+      },
+    },
+    urgencia: {
+      type: DataTypes.ENUM("alta", "media", "baixa"),
+      allowNull: false,
+      validate: {
+        isIn: [["alta", "media", "baixa"]], // Verifica se o valor está entre as opções permitidas
+      },
+    },
+  },
+  { paranoid: true, deletedAt: "destroyTime" }
+);
+
+Entregador.hasMany(Pedido);
+Pedido.hasOne(Entregador);
+
+module.exports = Pedido;
